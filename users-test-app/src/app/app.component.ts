a15439405921de8b5f8afdd8ee49561be791eda8
LindgrenArto <arto.lindgren1@gmail.com>
2024-11-15 06:46:38 +0200

    Added a sidenav

--- users-test-app/src/app/app.component.ts
+++ users-test-app/src/app/app.component.ts
@@ -7,21 +7,28 @@ import { UserStateService } from './user/services/user-state.service';
 import { UserService } from './user/services/user.service';
 import { User } from './user/models/user';
 import { ToolbarComponent } from "./user/ui/toolbar/toolbar.component";
+import { MatSidenavModule } from "@angular/material/sidenav";
+import { MatIconModule } from '@angular/material/icon';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatListModule } from '@angular/material/list';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, UserListComponent, UserDetailComponent, ToolbarComponent],
+  imports: [RouterOutlet,
+    UserListComponent,
+    UserDetailComponent,
+    ToolbarComponent,
+    MatSidenavModule,
+    MatIconModule,
+    MatDividerModule,
+    MatListModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 
 // For mocking purposes
 export class AppComponent implements OnInit {
-
-  onMenuClick() {
-    console.log('Menu clicked!');
-  }
 
   title = 'users-test-app';
   constructor(private userStateService: UserStateService,
