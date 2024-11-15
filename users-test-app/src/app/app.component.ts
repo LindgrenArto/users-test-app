55c2296cba866ee7141be08b42ec46694d7a5c89
LindgrenArto <arto.lindgren1@gmail.com>
2024-11-15 06:25:24 +0200

    Added toolbar to the app.component

--- users-test-app/src/app/app.component.ts
+++ users-test-app/src/app/app.component.ts
@@ -6,17 +6,23 @@ import { UserDetailComponent } from './user/user-detail/user-detail.component';
 import { UserStateService } from './user/services/user-state.service';
 import { UserService } from './user/services/user.service';
 import { User } from './user/models/user';
+import { ToolbarComponent } from "./user/ui/toolbar/toolbar.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, UserListComponent, UserDetailComponent],
+  imports: [RouterOutlet, UserListComponent, UserDetailComponent, ToolbarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 
 // For mocking purposes
 export class AppComponent implements OnInit {
+
+  onMenuClick() {
+    console.log('Menu clicked!');
+  }
+
   title = 'users-test-app';
   constructor(private userStateService: UserStateService,
     private userService: UserService) { }
